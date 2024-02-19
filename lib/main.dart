b1f703b6b76c4c68dde1b86da180cb77785ab621
Harshvardhan Singh Shekhawat <iamharsh.shekhawat@gmail.com>
2024-02-20 02:08:13 +0530

    Delete lib directory

--- lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:saarthi/features/regester_page/screen/regester_page.dart';
-
-void main() {
-  runApp(MaterialApp(
-    home: Regester(),
-  ));
-}
