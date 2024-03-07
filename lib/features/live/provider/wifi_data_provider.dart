7381ff232e048c36aed4a03f1fe3925172a97311
Amangupta0000 <amangupta1a2b3c@gmail.com>
2024-03-07 17:35:57 +0530

    wifi_data riverpod and local_storage class

---
+++ lib/features/live/provider/wifi_data_provider.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+import 'package:riverpod/riverpod.dart';
+import 'dart:io';
+
+import 'package:sarthi/models/wifi_model.dart';
+
+final socketProvider = StateProvider<Wifi_data?>((ref) => null);
+final socketDataProvider = StateProvider((ref) async {
+  final socket =
+      await Socket.connect('192.168.179.98', 8080); // Example socket connection
+  var buffer = '';
+  print('connecting..');
+  socket.listen(
+    (List<int> event) {
+      final data = utf8.decode(event); // Decode incoming bytes to string
+      buffer += data; // Append the received data to the buffer
+
+      // Continuously parse complete JSON objects from the buffer
+      while (buffer.contains(' ')) {
+        final newlineIndex = buffer.indexOf('\n');
+        final jsonString = buffer.substring(0, newlineIndex);
+
+        // Parse the JSON string
+        try {
+          final jsonMap = json.decode(jsonString);
+          //  setState(() {
+          //     wifi_data = Wifi_data.fromJson(jsonMap);
+          //  });
+         
+          ref.watch(socketProvider.notifier).state = Wifi_data.fromJson(jsonMap);   
+          
+          
+          print('Received JSON: $jsonMap');
+        } catch (e) {
+          print('Error parsing JSON: $e');
+        }
+
+        // Remove the parsed JSON object from the buffer
+        buffer = buffer.substring(newlineIndex + 1);
+      }
+    },
+  ).onData((data) {});
+});
---
+++ lib/local_storage/local_storage_class.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/foundation.dart';
+import 'package:sarthi/models/wifi_model.dart';
+import 'package:sqflite/sqflite.dart' as sql;
+
+class SQLHelper {
+  static Future<void> createTables(sql.Database database) async {
+    await database.execute("""CREATE TABLE items(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        aAcc1 TEXT
+        createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      )
+      """);
+  }
+
+  static Future<sql.Database> db() async {
+    return sql.openDatabase(
+      'measurement_data',
+      version: 1,
+      onCreate: (sql.Database database, int version) async {
+        await createTables(database);
+      },
+    );
+  }
+
+  // Create new item (wifi_data)
+  static Future<int> createItem(double? acc1 , Map< String, dynamic> wifi_data) async {
+    final db = await SQLHelper.db();
+   
+    final data = { 'acc1' : acc1};
+    final id = await db.insert('items', data,
+        // conflictAlgorithm: sql.ConflictAlgorithm.replace
+        );
+    return id;
+  }
+  // Read all items (wifi_data)
+  static Future<List<Map<String, dynamic>>> getItems() async {
+    final db = await SQLHelper.db();
+    return db.query('items', orderBy: "id");
+  }
+
+}
